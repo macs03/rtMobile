30ed3f9370db7d3714f9713030f8c87dca7d39d2
Miguel Cardenas <miguelcarde.3@gmail.com>
2016-07-07 11:26:41 -0400

    base of app

---
+++ rtMobile/www/js/routes.js
@@ -0,0 +1,53 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.search', {
+    url: '/',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/search.html',
+        controller: 'searchCtrl'
+      }
+    }
+  })
+
+  .state('menu.login', {
+    url: '/login',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      }
+    }
+  })
+
+  .state('menu.register', {
+    url: '/signup',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/register.html',
+        controller: 'registerCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    abstract:true
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/')
+
+  
+
+});
